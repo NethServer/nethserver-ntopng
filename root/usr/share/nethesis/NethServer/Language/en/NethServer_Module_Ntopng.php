58652eee2f6aa6067a22aeba11caedcabfae68d7
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2013-08-01 15:20:19 +0200

    translation: fix typo. Refs #1126

--- root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_Ntopng.php
+++ root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_Ntopng.php
@@ -6,4 +6,4 @@ $L['username_label'] = 'Username';
 $L['Password_label'] = 'Password for user \'admin\''; 
 $L['TCPPort_label'] = 'Port'; 
 $L['Ntopng_Tags'] = 'ntop bandwidth monitor usage'; 
-$L['Ntopng_Title'] = 'Bandwith monitor'; 
+$L['Ntopng_Title'] = 'Bandwidth monitor'; 
--- root/usr/share/nethesis/NethServer/Language/it/NethServer_Module_Ntopng.php
+++ root/usr/share/nethesis/NethServer/Language/it/NethServer_Module_Ntopng.php
@@ -5,5 +5,5 @@ $L['ntopng_title'] = 'Monitor utilizzo banda';
 $L['username_label'] = 'Username'; 
 $L['Password_label'] = 'Password per l\'utente \'admin\''; 
 $L['TCPPort_label'] = 'Porta';
-$L['Ntopng_Tags'] = 'ntop utilizzo banda monitor'; 
+$L['Ntopng_Tags'] = 'ntop utilizzo banda monitor bandwidth'; 
 $L['Ntopng_Title'] = 'Utilizzo banda'; 
