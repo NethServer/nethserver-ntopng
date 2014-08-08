645955543dbaf4d1936af83ac72be70e20aeecd9
Davide Principi <davide.principi@nethesis.it>
2014-08-08 13:05:04 +0200

    Ntopng: build Ntopng URL from HTTP Host header. Refs #2506
    Original patch from Filippo Carletti <filippo.carletti@nethesis.it>
    http://dev.nethserver.org/issues/2506#note-23

--- root/usr/share/nethesis/NethServer/Template/Ntopng.php
+++ root/usr/share/nethesis/NethServer/Template/Ntopng.php
@@ -1,5 +1,8 @@
 <?php
 
+$host = explode(':',$_SERVER['HTTP_HOST']);
+$url = "http://".$host[0].":".$view['TCPPort'];
+
 echo $view->header()->setAttribute('template', $T('ntopng_title'));
 
 echo $view->fieldsetSwitch('status', 'enabled',  $view::FIELDSETSWITCH_CHECKBOX)
@@ -10,10 +13,8 @@ echo $view->fieldsetSwitch('status', 'enabled',  $view::FIELDSETSWITCH_CHECKBOX)
         ->insert($view->textLabel()->setAttribute('template',$T('username_label').": admin"))
         ->insert($view->textInput('Password'))
     )
-    ->insert($view->fieldsetSwitch('Authentication', 'disabled'));
-
-
-$url = "http://".$view['FQDN'].":".$view['TCPPort'];
-echo "<p style='margin-bottom: 5px'>URL: <a href='$url' target='_blank'>$url</a></p>";
+    ->insert($view->fieldsetSwitch('Authentication', 'disabled'))
+    ->insert($view->panel()->insert($view->literal("URL: <a href='$url' target='_blank'>$url</a>")->setAttribute('hsc', FALSE)))
+;
 
 echo $view->buttonList($view::BUTTON_SUBMIT | $view::BUTTON_HELP);
